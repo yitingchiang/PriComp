cd3b2f9bfa949ab1482e63a02253192b33ab1f43
yitingchiang <yitingchiang@gmail.com>
2021-11-17 21:06:38 +0900

    Add files via upload

--- src/smc.h
+++ src/smc.h
@@ -1,10 +1,15 @@
 #ifndef SMC
-
 #define SMC
 #include <stdint.h>
 #define ulong128 unsigned __int128
 #define long128 __int128
+#include <gmp.h>
+#include "two_parties/two_parties.h"
+#include "two_parties/cSFloat.h"
 
+/** 
+  define ALICE=1, BOB=2
+ */
 enum { ALICE=1,BOB};
 
 #endif
